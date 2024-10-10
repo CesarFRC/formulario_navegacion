5612f80e95e8699da504803f68ef73248b27f388
Cesar Fabian Reyes <cesar.reyes.cortinas12@gmail.com>
2024-10-10 17:34:51 -0600

    Se agrego docuemnto de php de la conexion

---
+++ conexion.php
@@ -0,0 +1,16 @@
+<?php
+// Datos de conexión
+$hostname = "bvvmzyyh4ulq7sn4jdqw-mysql.services.clever-cloud.com";
+$username = "urbanwgc48m1xqha"; // Nombre de usuario de MySQL
+$password = "gss4FQPVz9aOF4XRs6aV"; // Deja la contraseña vacía si usas XAMPP por defecto
+$dbname = "bvvmzyyh4ulq7sn4jdqw"; // Cambia por el nombre de tu base de datos
+
+// Crear la conexión
+$conn = new mysqli($hostname, $username, $password, $dbname);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+echo "Conexión exitosa";
+?>
